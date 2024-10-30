bd4d017e405833adc81848d5082ddd7b3c10691d
fareedmirzay <fareedmirzay@ymail.com>
2024-10-30 15:59:08 -0400

    final changes

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -8,7 +8,7 @@ const productRouter = express.Router();
 
 productRouter.post('/add',upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),addProduct);
 
-roductRouter.post('/update',upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),updateProduct);
+productRouter.post('/update',upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),updateProduct);
 
 productRouter.post("/remove",removeProduct);
 
